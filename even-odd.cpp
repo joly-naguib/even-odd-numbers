c16c069bed74dc406b43d451da12c6b9420aef3b
joly-naguib <122123328+joly-naguib@users.noreply.github.com>
2023-01-06 21:11:42 +0100

    Create even-odd.cpp

---
+++ even-odd.cpp
@@ -0,0 +1,32 @@
+#include <iostream>
+
+using namespace std;
+
+int main()
+void noodds(int arr[],int k)
+{
+for (int i=0;i<k;i++)
+{
+if(arr[i]%2==0)
+cout<<arr[i]<<"";
+}
+
+}
+
+{
+    int n;
+  int numbers[n] ;
+  cout<<"enter  the size of the array: ";
+  cin >> n;
+  cout<<"enter the numbers into the array: ";
+for(int i=0;i<n i++)
+   {
+       
+       cin<<numbers[i];
+   } 
+}
+{
+    
+  cout<<"the even numbers in the array are:  "
+    noodds(numbers,n);
+}
